f4851cf53627dd02a31fa855ad5893b2a61b3159
csmplatform <csmplatform@users.noreply.github.com>
2024-02-26 10:45:46 +0000

    Automatically update python client (ref: Cosmo-Tech/cosmotech-api@e1173e06f9305d802eba1933bac6814dd8fc1359)
    Co-authored-by: lalepee <lalepee@users.noreply.github.com>

--- cosmotech_api/model/dataset_source_type.py
+++ cosmotech_api/model/dataset_source_type.py
@@ -58,6 +58,7 @@ class DatasetSourceType(ModelSimple):
             'FILE': "File",
             'NONE': "None",
             'TWINCACHE': "Twincache",
+            'ETL': "ETL",
         },
     }
 
@@ -109,10 +110,10 @@ class DatasetSourceType(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", ]  # noqa: E501
+            args[0] (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", "ETL", ]  # noqa: E501
 
         Keyword Args:
-            value (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", ]  # noqa: E501
+            value (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", "ETL", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
@@ -199,10 +200,10 @@ class DatasetSourceType(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", ]  # noqa: E501
+            args[0] (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", "ETL", ]  # noqa: E501
 
         Keyword Args:
-            value (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", ]  # noqa: E501
+            value (str): the Dataset Source Type., must be one of ["ADT", "AzureStorage", "File", "None", "Twincache", "ETL", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
